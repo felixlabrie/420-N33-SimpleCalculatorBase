3697df13e5bac1b909d14cf68ad1a333ec11eb39
Marina Melnichuk <1431488@champlaincollege.qc.ca>
2020-09-14 19:30:34 -0400

    Display the result in human readable form

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -49,11 +49,35 @@ namespace SimpleCalculator
                 }
 
                 Console.WriteLine("Enter your desired operation out of the following: add or +, subtract or -, divide or /, multiply or * and power or ^ :"); 
-                string operation = Console.ReadLine();
+                string operation = Console.ReadLine().ToLower();
 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
                 Console.WriteLine(result);
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Your first number ");
+                sb.Append(firstNumber);
+                switch(operation)
+                {
+                    case "add":
+                    case "+":
+                        sb.Append(" plus ");
+                        break;
+                    case "subtact":
+                    case "-":
+                        sb.Append(" minus ");
+                        break;
+                    
+                    
+                }
+                sb.Append("and your second number ");
+                sb.Append(secondNumber);
+                sb.Append(" equals to ");
+                sb.Append(result);
+
+                Console.WriteLine(sb.ToString());
+                Console.ReadLine();
+             
 
             } catch (Exception ex)
             {
